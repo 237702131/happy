20677d9b06ee686db2c568e15917b5262e8cd933
liangbc <bicong.liang@51zouchuqu.com>
2018-04-16 11:58:49 +0800

    happy

--- src/main/java/com/liangbc/happy/controller/UserController.java
+++ src/main/java/com/liangbc/happy/controller/UserController.java
@@ -2,8 +2,6 @@ package com.liangbc.happy.controller;
 
 import com.liangbc.happy.domain.City;
 import com.liangbc.happy.service.CityService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -19,20 +17,17 @@ import javax.annotation.Resource;
 @RequestMapping(value = "/v1/user", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class UserController {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Resource
     private CityService cityService;
 
     @GetMapping(value = "/{id}")
     public Mono<City> findById(@PathVariable Integer id) {
-        logger.info("获取城市根据id id= {}", id);
         return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.findCityById(id)));
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public Flux<City> findAllCity() {
-        logger.info("获取所有城市");
         return Flux.create(cityFluxSink -> {
             cityService.findAllCity().forEach(city -> {
                 cityFluxSink.next(city);
@@ -43,21 +38,16 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.POST)
     public Mono<Integer> createCity(@RequestBody City city) {
-
-        logger.info("创建城市 city = {}", city.toString());
-
         return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.saveCity(city)));
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     public Mono<Integer> modifyCity(@RequestBody City city) {
-        logger.info("修改城市 city = {}", city.toString());
         return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.updateCity(city)));
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public Mono<Integer> modifyCity(@PathVariable("id") Integer id) {
-        logger.info("修改城市 id = {}", id);
         return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.deleteCity(id)));
     }
 }
