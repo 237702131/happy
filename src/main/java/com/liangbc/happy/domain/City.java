45ebc219487402a7ce13e2ea39dec3b07a87430f
liangbc <bicong.liang@51zouchuqu.com>
2018-04-16 11:04:37 +0800

    happy

--- src/main/java/com/liangbc/happy/domain/City.java
+++ src/main/java/com/liangbc/happy/domain/City.java
@@ -1,8 +1,6 @@
 package com.liangbc.happy.domain;
 
 import lombok.Data;
-import org.jeecgframework.poi.excel.annotation.Excel;
-import org.jeecgframework.poi.excel.annotation.ExcelTarget;
 
 /**
  * 城市实体类
@@ -10,30 +8,25 @@ import org.jeecgframework.poi.excel.annotation.ExcelTarget;
  * Created by bysocket on 09/29/2017.
  */
 @Data
-@ExcelTarget("cityEntity")
 public class City  implements java.io.Serializable  {
 
     /**
      * 城市编号
      */
-    @Excel(name = "城市编号")
     private Integer id;
 
     /**
      * 省份编号
      */
-    @Excel(name = "省份编号")
     private Integer provinceId;
 
     /**
      * 城市名称
      */
-    @Excel(name = "城市名称")
     private String cityName;
 
     /**
      * 描述
      */
-    @Excel(name = "描述")
     private String description;
 }
