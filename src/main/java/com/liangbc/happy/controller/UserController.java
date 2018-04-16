26413c2a17306e6b724282da0cba747a2b136b7b
liangbc <bicong.liang@51zouchuqu.com>
2018-04-16 11:25:07 +0800

    happy

--- src/main/java/com/liangbc/happy/controller/UserController.java
+++ src/main/java/com/liangbc/happy/controller/UserController.java
@@ -12,7 +12,6 @@ import reactor.core.publisher.Mono;
 import javax.annotation.Resource;
 
 /**
- *
  * @author liangbc
  * @date 2017/8/28
  */
@@ -27,11 +26,13 @@ public class UserController {
 
     @GetMapping(value = "/{id}")
     public Mono<City> findById(@PathVariable Integer id) {
+        logger.info("获取城市根据id id= {}", id);
         return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.findCityById(id)));
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public Flux<City> findAllCity() {
+        logger.info("获取所有城市");
         return Flux.create(cityFluxSink -> {
             cityService.findAllCity().forEach(city -> {
                 cityFluxSink.next(city);
@@ -42,16 +43,21 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.POST)
     public Mono<Integer> createCity(@RequestBody City city) {
+
+        logger.info("创建城市 city = {}", city.toString());
+
         return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.saveCity(city)));
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     public Mono<Integer> modifyCity(@RequestBody City city) {
+        logger.info("修改城市 city = {}", city.toString());
         return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.updateCity(city)));
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public Mono<Integer> modifyCity(@PathVariable("id") Integer id) {
+        logger.info("修改城市 id = {}", id);
         return Mono.create(cityMonoSink -> cityMonoSink.success(cityService.deleteCity(id)));
     }
 }
